1c1b7169fa8a51c7e04fb2d8dc9a8efebd56a8f7
Karla Gallegos <kgalleg618@gmail.com>
2019-10-24 09:48:53 -0500

    added my models

---
+++ aromableapp/models/recipe.py
@@ -0,0 +1,23 @@
+from django.db import models
+from .category import Category
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Recipe(models.Model):
+
+    name = models.CharField(max_length=50, blank=True, null=True)
+    notes = models.CharField(max_length=150, blank=True, null=True)
+
+    #foreign keys
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = ("recipe")
+        verbose_name_plural = ("recipes")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("recipe_detail", kwargs={"pk": self.pk})
