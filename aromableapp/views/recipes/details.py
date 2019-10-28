86ecf062a23ed520b164aa37df86fdb93f1331b6
Karla Gallegos <kgalleg618@gmail.com>
2019-10-28 12:18:36 -0500

    added register

--- aromableapp/views/recipes/details.py
+++ aromableapp/views/recipes/details.py
@@ -8,47 +8,10 @@ from ..connection import Connection
 
 
 
-def create_recipe(cursor, row):
-    _row = sqlite3.Row(cursor, row)
-
-    recipe = Recipe()
-    recipe.id = _row["recipe_id"]
-    recipe.name = _row["name"]
-    recipe.notes = _row["notes"]
-
-    # recipe.category_id = _row["category_id"]
-    # recipe.user_id = _row["user_id"]
-    # recipe.ingredients = []
-    # library.books = []
-    recipe.categories = []
-
-    ingredient = Ingredient()
-    ingredient.id = _row["ingredient_id"]
-    ingredient.name = _row["ingredient_name"]
-    ingredient.notes = _row["notes"]
-
-    recipeingredient = RecipeIngredient()
-    recipeingredient.id = _row["recipeingredient_id"]
-    # recipeingredient.quantity = _row["quantity"]
-    recipeingredient.ingredient_id = _row["ingredient_id"]
-    # recipeingredient.recipe_id = _row["recipe_id"]
-
-    category = Category()
-    category.id = _row["category_id"]
-    category.name = _row["category_name"]
-
-
-    recipe.ingredient = ingredient
-    recipe.category = category
-
-    return recipe
-
-
-
 def get_recipe(recipe_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = create_recipe
-        # conn.row_factory = model_factory(Recipe)
+
+        conn.row_factory = model_factory(Recipe)
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
